9d7ad802a3ef6a82c694465d0e8d80d49b0e6858
reham-elsyed <rehamshepl666@gmail.com>
2024-05-02 06:51:57 -0700

    https

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # install a specific version of flask (2.1.0)
-exec { 'install_flask_2.1.0':
-	command => 'pip3 install Flask==2.1.0',
-	path	=> ['/usr/local/bin', '/usr/bin'],
+package { 'flask':
+	ensure	 => '2.1.0',
+	provider => 'pip3',
 }
---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Configuration of connection to a server
+ssh -i ~/.ssh/school ubuntu@18.235.233.59
